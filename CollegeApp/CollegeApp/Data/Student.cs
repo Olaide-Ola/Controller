e0bda74df118dc0000a54731eb045bec771a96d3
Olaide Ogunbunmi <ogunbunmi.ridwan@yahoo.com>
2026-05-03 09:58:33 +0100

    Remove Email property from Student entity
    The Email field was deleted from the Student class in CollegeApp.Data. Student email addresses are no longer stored or managed as part of the Student entity.

--- CollegeApp/CollegeApp/Data/Student.cs
+++ CollegeApp/CollegeApp/Data/Student.cs
@@ -9,7 +9,6 @@ namespace CollegeApp.Data
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } = 0;
         public string StudentName { get; set; }
-
         public string Email { get; set; }
         public string Address { get; set; }
         public DateTime DOB { get; set; }
